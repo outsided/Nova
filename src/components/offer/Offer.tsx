1529ca876161ae8fa69940074881a1ee09d47742
Igainst <antimagdota2@mial.ru>
2024-02-02 18:31:13 +0300

    dasd

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -1,17 +1,21 @@
+import Image from 'next/image'
+import {OfferIcon} from './OfferIcon'
 import styles from './styles.module.css'
 export const Offer = () => {
 	return (
 		<section className='w-full'>
 			<div className={styles.container}>
 				<h2 className='text-[64px] font-bold '>We offer</h2>
-				<div className={styles.butto_1}><button ><img></img>1</button></div>
-				<div className={styles.butto_2}><button >2</button></div>
-				<div className={styles.butto_3}><button >3</button></div>
-				<div className={styles.butto_3}><button >4</button></div>
-				<div className={styles.butto_3}><button >5</button></div>
-				<div className={styles.butto_2}><button >6</button></div>
-				<div className={styles.butto_3}><button >7</button></div>
-				<div className={styles.butto_1}><button >8</button></div>				
+				<OfferIcon />
+				<div className={styles.butto_1}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_2}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_3}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_3}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_3}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_2}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_3}><button ><Image src='' alt=''/></button></div>
+				<div className={styles.butto_1}><button ><Image src='' alt=''/></button></div>	
+
 			</div>	
 		</section>
 	)
