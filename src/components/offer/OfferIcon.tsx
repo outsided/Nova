1b5ff228e1adfe937979475288eaaeb06c8c5332
Igainst <antimagdota2@mial.ru>
2024-02-09 15:53:01 +0300

    ty

--- src/components/offer/OfferIcon.tsx
+++ src/components/offer/OfferIcon.tsx
@@ -2,7 +2,7 @@ export const OfferIcon = () => {
 	return (
 		<div >
 				<div className='absolute top-0 left-[-59%] right-0 bottom-0 bg-[url("../assets/wave.png")] bg-no-repeat bg-top opacity-20 -z-10 h-[1190px]'></div>
-				<div className='absolute w-[100px] left-[-10%] top-[40%] bg-[#8F15FF] shadow-inner  rounded-full h-[100px] drop-shadow-[0_35px_35px_rgba(255,0,255)]' ></div>
+				<div className='absolute w-[100px] left-[-10%] top-[30%] bg-[#8F15FF] shadow-inner  rounded-full h-[400px] drop-shadow-[200px_0px_150px_#8F15FF]' ></div>
 		</div>
 	)
 }
